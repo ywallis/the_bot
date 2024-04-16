a8241e4e3fdb9c2284309ad34ad3597d02af3f35
yann <yann.wallis@gmail.com>
2024-04-16 20:59:28 +0200

    Implemented first version of one-sided buy maker

--- make1.py
+++ make1.py
@@ -47,29 +47,60 @@ def overwatch(client_a, client_b, pair, spread):
             if not buy_exists:
                 print(f'Make on {client_b.name}')
                 print(f'Buy {bid_b}')
+
+                # FICTITIOUS PRICE
+
+                buy_order = client_b.create_limit_buy_order(symbol=pair, amount=10, price=1)
                 buy_exists = True
             else:
                 print('Buy order already present')
+                buy_filled = client_b.fetch_order(id=buy_order['id'], symbol=pair)['filled']
+                print(f'{buy_filled} from order filled')
                 # retrieve order
-                # market sell any filled
-                # if not top bid cancel
+                if buy_filled != 0:
+
+                    # ADD MARKET SELL FOR PRODUCTION
+                    # market sell any filled on A
+
+                    # client_a.create_market_order(symbol=pair, side='sell', amount=buy_filled)
+                    print(f'Market selling {buy_filled} {pair} on {client_b.name}')
+                    client_b.cancel_order(id=buy_order['id'], symbol=pair)
+                    buy_exists = False
+
+                if buy_order['price'] != bid_b:
+                    # if not top bid cancel
+
+                    print('Order no longer at top of bids, cancelling.')
+
+                    client_b.cancel_order(id=buy_order['id'], symbol=pair)
+
+                    buy_exists = False
 
         else:
             # retrieve order
             # market sell any filled
-            print('Cancel all')
-            sell_exists = False
-            buy_exists = False
+            if buy_exists:
+                print('Cancel all')
+                buy_filled = client_b.fetch_order(id=buy_order['id'], symbol=pair)
+                print(f'{buy_filled} from order filled')
+                # retrieve order
+                if buy_filled != 0:
+                    # client_a.create_market_order(symbol=pair, side='sell', amount=buy_filled)
+                    print(f'Market selling {buy_filled} {pair} on {client_b.name}')
+                client_b.cancel_order(id=buy_order['id'], symbol=pair)
+                print(f'Buy order cancelled on {client_b.name}')
+                sell_exists = False
+                buy_exists = False
 
 
-# overwatch(gate_maker, mexc_maker, 'ALPH/USDT', 1.0025)
+# overwatch(gate_maker, mexc_maker, 'ALPH/USDT', 1)
 
-# print(gate_maker.fetch_open_orders('ALPH/USDT'))
 #
 # print(gate_maker.fetch_order(id='558708798916', symbol='ALPH/USDT'))
 
-order = gate_maker.create_limit_buy_order('ALPH/USDT', 100, 1)
-print(order)
-time.sleep(5)
-print(gate_maker.fetch_order(id=order['id'], symbol='ALPH/USDT')['filled'])
-gate_maker.cancel_order(id=order['id'], symbol='ALPH/USDT')
+# order = gate_maker.create_limit_buy_order('ALPH/USDT', 100, 1)
+# print(order)
+# time.sleep(5)
+# print(gate_maker.fetch_order(id=order['id'], symbol='ALPH/USDT')['filled'])
+# mexc_maker.cancel_order(id='C02__408987254439067648008', symbol='ALPH/USDT')
+# print(mexc_maker.fetch_open_orders('ALPH/USDT'))
