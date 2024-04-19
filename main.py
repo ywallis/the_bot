5a677f3af33482e90a61daf550e187426a3834a9
yann <yann.wallis@gmail.com>
2024-04-19 22:05:32 +0200

    Corrected exception bug

--- main.py
+++ main.py
@@ -71,5 +71,5 @@ if __name__ == '__main__':
         except requests.exceptions.HTTPError:
             print('HTTP Error')
 
-        except ccxt.base.errors.NetworkError as e:
+        except ccxt.NetworkError as e:
             print('Network Error')
