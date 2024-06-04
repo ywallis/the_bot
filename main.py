e5321c03d4c337650e572912b8e62ae73a7c9cde
Yann <146178938+ywallis@users.noreply.github.com>
2024-06-04 08:18:32 +0200

    Implemented market buy logic for MEXC involving cost.

--- main.py
+++ main.py
@@ -12,29 +12,28 @@ logger = logging.getLogger(__name__)
 
 making = True
 
-# if __name__ == '__main__':
-#
-#     while making is True:
-#         try:
-#             make_and_take(taker_client=taker_client, maker_client=maker_client, pair=pair, maker_spread=maker_spread,
-#                           maker_size=maker_size, taker_spread=taker_min_spread,
-#                           taker_sizing=taker_sizing, taker_max_order_size=taker_max_order_size)
-#
-#         except ccxt.NetworkError as e:
-#             print('Network error')
-#             logger.info('Network error')
-#             logger.info(e)
-#
-#         except ccxt.ExchangeError as e:
-#
-#             # Has happened because of too many requests.
-#             time.sleep(5)
-#             print('Exchange error')
-#             logger.info(e)
+if __name__ == '__main__':
+
+    while making is True:
+        try:
+            make_and_take(taker_client=taker_client, maker_client=maker_client, pair=pair, maker_spread=maker_spread,
+                          maker_size=maker_size, taker_spread=taker_min_spread,
+                          taker_sizing=taker_sizing, taker_max_order_size=taker_max_order_size)
+
+        except ccxt.NetworkError as e:
+            print('Network error')
+            logger.info('Network error')
+            logger.info(e)
+
+        except ccxt.ExchangeError as e:
+
+            # Has happened because of too many requests.
+            time.sleep(5)
+            print('Exchange error')
+            logger.info(e)
 
 
 # # In case of emergencies, kill all open orders on maker client.
 # Replace this by an entry in config for selection?
 # maker_client.cancel_all_orders('ALPH/USDT')
 
-maker_client.create_market_buy_order_with_cost('ALPH/USDT', 10)
