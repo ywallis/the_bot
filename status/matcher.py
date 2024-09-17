039d72a1e2eb49a0c2e7babee31b397b9f6a0c91
Yann <yann.wallis@gmail.com>
2024-09-17 10:36:36 +0800

    Introduced matcher to help with trade reconciliation

---
+++ status/matcher.py
@@ -0,0 +1,23 @@
+from config.config import gate_client, mexc_client, bitget_client, pair
+import pandas as pd
+
+### First version, fetches all open orders and exports them to a csv with matching-relevant information.
+
+orders = []
+
+clients = [gate_client, mexc_client, bitget_client]
+
+for client in clients:
+    client_orders = client.fetch_open_orders(pair)
+
+    for order in client_orders:
+        formatted = {'id': order['clientOrderId'], 'price': order['price'], 'amount': order['amount'],
+                     'side': order['side'],
+                     'remaining': order['remaining'], 'exchange': client.name}
+        orders.append(formatted)
+        print(formatted)
+
+df = pd.DataFrame(orders)
+df.set_index('id', inplace=True)
+print(df)
+df.to_csv('matching.csv')
