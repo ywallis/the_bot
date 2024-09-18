e4dadc209706537d7e1d4def3bd8cdf9ead3411d
Yann <yann.wallis@gmail.com>
2024-09-18 15:39:01 +0800

    Changed directory formatting to allow compatibility with python < 3.12

--- status/db_refresh.py
+++ status/db_refresh.py
@@ -9,7 +9,7 @@ def refresh_db(db_name):
     """This function takes in the name of a DB (trades or orders) and refreshes it to include all downloaded values.
     As of SEP24, values can either be 'Trades' or 'Orders'."""
 
-    directory = f'{path_to_data}{pair.split('/')[0]}/{db_name}/'
+    directory = f'{path_to_data}{pair.split("/")[0]}/{db_name}/'
 
     dfs = []
     for filename in os.listdir(directory):
