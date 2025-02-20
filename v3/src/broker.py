df0cc680609c07796c261ab04ef4ccde6fab7bcc
ywallis <yann.wallis@gmail.com>
2025-02-20 12:56:23 +0800

    finalized broker confirmation flow

--- v3/src/broker.py
+++ v3/src/broker.py
@@ -64,15 +64,20 @@ async def worker(exchange_name: str, queue: asyncio.Queue, ccxt_client: CustomEx
                     await asyncio.sleep(0.01)  # Simulate async processing
                     confirmation = "fun"
             
-                ### I NEED A BETTER FLOW FOR THIS!!
+                else:
+                    logger.error(f"Message of unknown type was allowed through: {data}")
+                    raise BrokerError(f"Message of unknown type was allowed through: {data}")
 
-                await redis_out.publish(data['id'], f"Reply from broker for {data['id']} on {exchange_name} is {confirmation}")
 
             except BrokerError as e:
-                # Catch  error here to notify order manager
                 
-                confirmation = e
+                
+                await redis_out.publish(data['id'], f"Error for {data['id']} on {exchange_name} is {e}")
             
+            else:
+
+                # Happy flow
+                await redis_out.publish(data['id'], f"Reply from broker for {data['id']} on {exchange_name} is {confirmation}")
             
             queue.task_done()
 
