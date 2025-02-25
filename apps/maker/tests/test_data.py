e3fe41ed5cb7c55bdc1a93ea47a9ec38c94e31ad
ywallis <yann.wallis@gmail.com>
2025-02-25 22:40:54 +0800

    Formatting change for TOML files

--- apps/maker/tests/test_data.py
+++ apps/maker/tests/test_data.py
@@ -15,66 +15,66 @@ order_raw: dict[str, str] = {
     "amount": "10",
 } 
 
-order_1: OrderMessage = {
-    "kind": MessageType.ORDER,
-    "strategy": "ALPH_gate",
-    "exchange": "gate",
-    "id": "gate_test_1",
-    "exchange_id": "_",
-    "pair": "ALPH/USDT",
-    "side": OrderSide.SELL,
-    "price": Decimal(100),
-    "amount": Decimal(10),
-}
-
-order_2: OrderMessage = {
-    "kind": MessageType.ORDER,
-    "strategy": "ALPH_gate",
-    "exchange": "gate",
-    "id": "gate_test_2",
-    "exchange_id": "_",
-    "pair": "ALPH/USDT",
-    "side": OrderSide.SELL,
-    "price": Decimal(100),
-    "amount": Decimal(10),
-}
-
-cancellation_1: CancellationMessage = {
-    "kind": MessageType.CANCELLATION,
-    "strategy": "ALPH_gate",
-    "exchange": "gate",
-    "id": order_2["id"],
-    "pair": "ALPH/USDT",
-}
-
-# order_1: OrderMessage = OrderMessage(
-#     kind=MessageType.ORDER,
-#     strategy="ALPH_gate",
-#     exchange="gate",
-#     id="gate_test_1",
-#     exchange_id="_",
-#     pair="ALPH/USDT",
-#     side=OrderSide.SELL,
-#     price=Decimal(100),
-#     amount=Decimal(10),
-# )
+# order_1: OrderMessage = {
+#     "kind": MessageType.ORDER,
+#     "strategy": "ALPH_gate",
+#     "exchange": "gate",
+#     "id": "gate_test_1",
+#     "exchange_id": "_",
+#     "pair": "ALPH/USDT",
+#     "side": OrderSide.SELL,
+#     "price": Decimal(100),
+#     "amount": Decimal(10),
+# }
 #
-# order_2: OrderMessage = OrderMessage(
-#     kind=MessageType.ORDER,
-#     strategy="ALPH_gate",
-#     exchange="gate",
-#     id="gate_test_2",
-#     exchange_id="_",
-#     pair="ALPH/USDT",
-#     side=OrderSide.SELL,
-#     price=Decimal(100),
-#     amount=Decimal(10),
-# )
+# order_2: OrderMessage = {
+#     "kind": MessageType.ORDER,
+#     "strategy": "ALPH_gate",
+#     "exchange": "gate",
+#     "id": "gate_test_2",
+#     "exchange_id": "_",
+#     "pair": "ALPH/USDT",
+#     "side": OrderSide.SELL,
+#     "price": Decimal(100),
+#     "amount": Decimal(10),
+# }
 #
-# cancellation_1: CancellationMessage = CancellationMessage(
-#     kind=MessageType.CANCELLATION,
-#     strategy="ALPH_gate",
-#     exchange="gate",
-#     id=order_2["id"],
-#     pair="ALPH/USDT",
-# )
+# cancellation_1: CancellationMessage = {
+#     "kind": MessageType.CANCELLATION,
+#     "strategy": "ALPH_gate",
+#     "exchange": "gate",
+#     "id": order_2["id"],
+#     "pair": "ALPH/USDT",
+# }
+
+order_1: OrderMessage = OrderMessage(
+    kind=MessageType.ORDER,
+    strategy="ALPH_gate",
+    exchange="gate",
+    id="gate_test_1",
+    exchange_id="_",
+    pair="ALPH/USDT",
+    side=OrderSide.SELL,
+    price=Decimal(100),
+    amount=Decimal(10),
+)
+
+order_2: OrderMessage = OrderMessage(
+    kind=MessageType.ORDER,
+    strategy="ALPH_gate",
+    exchange="gate",
+    id="gate_test_2",
+    exchange_id="_",
+    pair="ALPH/USDT",
+    side=OrderSide.SELL,
+    price=Decimal(100),
+    amount=Decimal(10),
+)
+
+cancellation_1: CancellationMessage = CancellationMessage(
+    kind=MessageType.CANCELLATION,
+    strategy="ALPH_gate",
+    exchange="gate",
+    id=order_2["id"],
+    pair="ALPH/USDT",
+)
