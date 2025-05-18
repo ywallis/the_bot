0a4722db00b798f29feb75d73cd0eaa354cf0007
ywallis <yann.wallis@gmail.com>
2025-05-18 14:54:32 +0800

    increasing lag at broker startup

--- apps/maker/src/message_processor.py
+++ apps/maker/src/message_processor.py
@@ -300,7 +300,7 @@ async def main():
 
     # Let broker boot and check for open orders
 
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     processor.open_orders = await processor.get_open_orders()
     if processor.open_orders:
         logger.info(f"Pre-existing open order were found: {processor.open_orders}")
