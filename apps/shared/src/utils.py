652f3f87be9965b3f5ed121ffaac503e60c173ea
ywallis <yann.wallis@gmail.com>
2025-05-19 11:46:01 +0800

    added differentiation between test and prod strategies

--- apps/shared/src/utils.py
+++ apps/shared/src/utils.py
@@ -1,5 +1,7 @@
+import os
 import tomllib
 from pathlib import Path
+from dotenv import load_dotenv
 
 # TODO:
 # - Separate config for dev and prod
@@ -12,12 +14,19 @@ def load_config():
         return config
 
 
+# Load environment variables
+load_dotenv()
+production = os.getenv("TESTING", False) != "True"
+if production:
+    print("WARNING: USING PROD STRATEGIES")
+
 exchange_and_pair: set[tuple[str, str]] = set()
 pairs: set[str] = set()
 
 strategies: list[dict[str, str]] | None = load_config().get("strategies")
 if strategies is None:
     raise Exception("No strategy found")
+strategies = list(filter(lambda x: x["production"] == production, strategies))
 for strategy in strategies:
     exchange_and_pair.add((strategy["exchange_1"], strategy["symbol"]))
     exchange_and_pair.add((strategy["exchange_2"], strategy["symbol"]))
