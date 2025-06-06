5d5cdfb0ab7764e580a6201bfe4b822239223bef
ywallis <yann.wallis@gmail.com>
2025-06-06 10:47:34 +0200

    Added unsubscribe errors to watcher

--- apps/shared/src/errors.py
+++ apps/shared/src/errors.py
@@ -1,6 +1,7 @@
 from ccxt.async_support import BadRequest as BadRequest
 from ccxt.async_support import ExchangeError as ExchangeError
+from ccxt.async_support import InvalidNonce as InvalidNonce
+from ccxt.async_support import InvalidOrder as InvalidOrder
 from ccxt.async_support import NetworkError as NetworkError
-from ccxt.async_support import InvalidOrder as InvalidOrder 
 from ccxt.async_support import RequestTimeout as RequestTimeout
-from ccxt.async_support import InvalidNonce as InvalidNonce 
+from ccxt.async_support import UnsubscribeError as UnsubscribeError
