77863abefef2836176e699a3de781ecb95fabdc6
ywallis <yann.wallis@gmail.com>
2025-06-06 12:02:11 +0200

    got started on new fake maker strat

---
+++ apps/maker/src/strategies/fake_maker.py
@@ -0,0 +1,115 @@
+import asyncio
+import logging
+import time
+
+from redis.asyncio import Redis
+
+import apps.shared.src.logging_config as logging_config
+from apps.maker.src.enums import OrderSide
+from apps.maker.src.strategies.utils import (
+    generate_order_replace,
+    maker_order_sizer,
+    min_max_usd_converter,
+    retrieve_ob_redis,
+    send_processor_cancellation,
+    within_percentage_range,
+)
+from apps.maker.src.structs import OrderMessage
+
+logging_config.setup_logging()
+logger = logging.getLogger(__name__)
+
+# TODO:
+# - Consider which throttling systems still make sense
+# - Make sure all values put into redis follow the format I want
+
+
+async def fake_maker(redis: Redis, strategy: dict[str, str]):
+    # Defining state
+
+    watching: bool = True
+
+    symbol: str = strategy["symbol"]
+    maker: str = strategy["maker_exchange"]
+    taker: str = strategy["taker_exchange"]
+    spread: float = float(strategy["spread"])
+    min_spread: float = float(strategy["min_spread"])
+    min_size_usdt: float = float(strategy["min_size_usdt"])
+    max_size_usdt: float = float(strategy["max_size_usdt"])
+    liquidity_utilization: float = float(strategy["liquidity_utilization"])
+    min_size: float
+    max_size: float
+    order_side: OrderSide | None = None
+    sell_order: OrderMessage | None = None
+    buy_order: OrderMessage | None = None
+    sell_order_identifier: str = "es"
+    buy_order_identifier: str = "eb"
+
+    # Cancels any open orders in case process has to restart
+
+    await send_processor_cancellation(redis, strategy, sell_order_identifier)
+    await send_processor_cancellation(redis, strategy, buy_order_identifier)
+
+    while watching:
+        # Simple throttle
+
+        time.sleep(0.01)
+
+        batch = asyncio.gather(
+            retrieve_ob_redis(redis, f"{symbol}-{maker}"),
+            retrieve_ob_redis(redis, f"{symbol}-{taker}"),
+        )
+        maker_order_book, taker_order_book = await batch
+        if maker_order_book is None:
+            logger.debug(f"Could not fetch order book for {maker}")
+            await asyncio.sleep(1)
+            continue
+
+        if taker_order_book is None:
+            logger.debug(f"Could not fetch order book for {taker}")
+            await asyncio.sleep(1)
+            continue
+
+        logger.debug(f"Taker ({taker}) order book is \n {taker_order_book}")
+        logger.debug(f"Maker ({maker}) order book is \n {maker_order_book}")
+
+        taker_client_bids: list[list[float]] = taker_order_book["bids"]
+        taker_client_asks: list[list[float]] = taker_order_book["asks"]
+        maker_client_bids: list[list[float]] = maker_order_book["bids"]
+        maker_client_asks: list[list[float]] = maker_order_book["asks"]
+
+        best_bid_taker: float = taker_client_bids[0][0]
+        best_ask_taker: float = taker_client_asks[0][0]
+        best_bid_maker: float = maker_client_bids[0][0]
+        best_ask_maker: float = maker_client_asks[0][0]
+
+        min_size, max_size = min_max_usd_converter(
+            best_bid_taker, min_size_usdt, max_size_usdt
+        )
+
+        target_sell_price: float = best_ask_taker * spread
+        target_buy_price: float = best_bid_taker * spread
+
+        # Sell order generation
+
+        available_liquidity_taker = maker_order_sizer(
+            target_sell_price,
+            taker_client_asks,
+            OrderSide.SELL,
+            min_spread,
+            max_size,
+        )
+
+        available_liquidity_maker = maker_order_sizer(
+            target_sell_price,
+            maker_client_asks,
+            OrderSide.SELL,
+            min_spread,
+            max_size,
+        )
+
+        max_sell_size = available_liquidity_taker * liquidity_utilization
+
+        if max_sell_size > min_size:
+            pass
+            # generate_order_replace()
